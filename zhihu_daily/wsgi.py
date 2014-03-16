2796b57080765271b6b588c8b3052085804e8be7
liyuntao <liyuntao58607@gmail.com>
2014-03-17 02:10:29 +0800

    no message

--- zhihu_daily/wsgi.py
+++ zhihu_daily/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.abspath(os.path.join(root_path, 'zhihu_daily')))
+sys.path.insert(0, root_path)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zhihu_daily.settings")
 
 from django.core.wsgi import get_wsgi_application
